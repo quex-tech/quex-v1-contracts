22b62f7e5a41a2531d75ea21076aded569a86a1b
Aleksandr Bannikov <alexdinaro@gmail.com>
2024-10-23 12:21:05 +0200

    Add event on request spec enities added

--- contracts/V1RequestSpecRegistry.sol
+++ contracts/V1RequestSpecRegistry.sol
@@ -4,6 +4,12 @@ pragma solidity 0.8.22;
 import "../interfaces/IV1RequestSpecRegistry.sol";
 
 contract V1RequestSpecRegistry is IV1RequestSpecRegistry {
+    event RequestAdded(bytes32 requestId);
+    event PrivatePatchAdded(bytes32 patchId);
+    event JqFilterAdded(bytes32 filterId);
+    event ResultSchemaAdded(bytes32 schemaId);
+    event RequestSpecAdded(bytes32 requestSpecId);
+
     struct RequestSpecInternal {
         bytes32 requestId;
         bytes32 patchId;
@@ -21,24 +27,28 @@ contract V1RequestSpecRegistry is IV1RequestSpecRegistry {
     function addRequest(HTTPRequest memory request) external returns (bytes32 requestId) {
         requestId = keccak256(abi.encode(request));
         requests[requestId] = request;
+        emit RequestAdded(requestId);
         return requestId;
     }
 
     function addPrivatePatch(uint256 tdId, HTTPPrivatePatch memory privatePatch) external returns (bytes32 patchId) {
         patchId = keccak256(abi.encodePacked(tdId, abi.encode(privatePatch)));
         privatePatches[patchId] = privatePatch;
+        emit PrivatePatchAdded(patchId);
         return patchId;
     }
 
     function addJqFilter(string memory jqFilter) external returns (bytes32 filterId) {
         filterId = keccak256(bytes(jqFilter));
         jqFilters[filterId] = jqFilter;
+        emit JqFilterAdded(filterId);
         return filterId;
     }
 
     function addResponseSchema(string memory responseSchema) external returns (bytes32 schemaId) {
         schemaId = keccak256(bytes(responseSchema));
         resultSchemas[schemaId] = responseSchema;
+        emit ResultSchemaAdded(schemaId);
         return schemaId;
     }
 
@@ -51,6 +61,7 @@ contract V1RequestSpecRegistry is IV1RequestSpecRegistry {
         RequestSpecInternal memory requestSpecInternal = RequestSpecInternal(requestId, patchId, filterId, schemaId);
         requestSpecId = keccak256(abi.encode(requestSpecInternal));
         requestSpecs[requestSpecId] = requestSpecInternal;
+        emit RequestSpecAdded(requestSpecId);
         return requestSpecId;
     }
 
