33a7f3304a51d471de18359caa368c94713ebfad
Aleksandr Bannikov <alexdinaro@gmail.com>
2025-01-06 20:26:12 +0100

    Oracle pools. Add common facets for constant prices and treasuries

---
+++ contracts/facets/oracles/common/monetary_constant_price/ConstantPriceMonetaryFacet.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.22;
+
+import "./ConstantPriceMonetaryStorage.sol";
+import "@solidstate/contracts/access/ownable/Ownable.sol";
+
+contract ConstantPriceMonetaryFacet is Ownable {
+    function getActionFee(uint256 actionId) external view returns (uint256) {
+        return ConstantPriceMonetaryStorage.layout().actionFee;
+    }
+
+    function setActionFee(uint256 fee) external onlyOwner {
+        ConstantPriceMonetaryStorage.layout().actionFee = fee;
+    }
+}
