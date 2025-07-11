2d8079798e6b895cdafa8fe2360b97300e89e8c9
Aleksandr Bannikov <alexdinaro@gmail.com>
2025-07-11 14:05:19 +0200

    Fix state-visibility

--- contracts/facets/p256_verifier/P256VerifierFacet.sol
+++ contracts/facets/p256_verifier/P256VerifierFacet.sol
@@ -13,27 +13,27 @@ import {IP256Verifier} from "../../interfaces/core/IP256Verifier.sol";
 contract P256VerifierFacet is IP256Verifier {
     // Parameters for the sec256r1 (P256) elliptic curve
     // Curve prime field modulus
-    uint256 constant p =
+    uint256 private constant p =
         0xFFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFF;
     // Short weierstrass first coefficient
-    uint256 constant a = // The assumption a == -3 (mod p) is used throughout the codebase
+    uint256 private constant a = // The assumption a == -3 (mod p) is used throughout the codebase
         0xFFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFC;
     // Short weierstrass second coefficient
-    uint256 constant b =
+    uint256 private constant b =
         0x5AC635D8AA3A93E7B3EBBD55769886BC651D06B0CC53B0F63BCE3C3E27D2604B;
     // Generating point affine coordinates
-    uint256 constant GX =
+    uint256 private constant GX =
         0x6B17D1F2E12C4247F8BCE6E563A440F277037D812DEB33A0F4A13945D898C296;
-    uint256 constant GY =
+    uint256 private constant GY =
         0x4FE342E2FE1A7F9B8EE7EB4A7C0F9E162BCE33576B315ECECBB6406837BF51F5;
     // Curve order (number of points)
-    uint256 constant n =
+    uint256 private constant n =
         0xFFFFFFFF00000000FFFFFFFFFFFFFFFFBCE6FAADA7179E84F3B9CAC2FC632551;
     // -2 mod p constant, used to speed up inversion and doubling (avoid negation)
-    uint256 constant minus_2modp =
+    uint256 private constant minus_2modp =
         0xFFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFD;
     // -2 mod n constant, used to speed up inversion
-    uint256 constant minus_2modn =
+    uint256 private constant minus_2modn =
         0xFFFFFFFF00000000FFFFFFFFFFFFFFFFBCE6FAADA7179E84F3B9CAC2FC63254F;
 
     /**
