b4fcfd0b958af43faebf08f326b40ac1a81db693
Aleksandr Bannikov <alexdinaro@gmail.com>
2025-07-21 12:29:23 +0400

    Change quex treasury for kasplex testnet

--- scripts/quex_config.ts
+++ scripts/quex_config.ts
@@ -379,7 +379,7 @@ export const quexConfig: { [key: string]: QuexNetworkConfig } = {
         core: {
             quexFee: 1_000_000_000_000_000_000n, // 1 KAS
             quexFulfillingGasCost: 110_000n,
-            treasuryAddress: DEFAULT_TREASURY,
+            treasuryAddress: "0x70beA06316c51097dF67496feaFb7F50758019b9", // Relayer address
             managerAddress: DEFAULT_MANAGER
         },
         request: {
